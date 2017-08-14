abc6ccdc0a666d25e77e41aa6059df38c418a62f
Duy Nguyen <lthh89vt@gmail.com>
2017-08-14 10:59:05 +1000

    refactor: get & display property list from json file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Property} from './@models/property';
+import {Http} from '@angular/http';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  private propertiesDataUrl = '/assets/data/properties.json';
+  results: Property[] = [];
+  savedProperty: Property[] = [];
+
+  /**
+   * Constructor
+   */
+  constructor(private http: Http) {}
+
+  /**
+   * Initialisation
+   */
+  ngOnInit(): void {
+    // Get property data from json file and add to results & saved properties
+    this.http.get(this.propertiesDataUrl).toPromise().then(response => {
+        response = response.json();
+        response['results'].map(propertyData => {
+            this.results.push(new Property(propertyData));
+        });
+        response['saved'].map(propertyData => {
+            this.savedProperty.push(new Property(propertyData));
+        });
+    }).catch(
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/property-card/property-card.component.ts
+++ src/app/property-card/property-card.component.ts
@@ -1,25 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Property} from '../@models/property';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.scss']
 })
-export class PropertyCardComponent implements OnInit {
-  propertyDetail = {
-    price: '$726,500',
-    agency: {
-        brandingColors: {
-            primary: '#ffe512'
-        },
-        logo: 'http://i1.au.reastatic.net/agencylogo/XRWXMT/12/20120927204448.gif'
-    },
-    id: '1',
-    mainImage: 'http://i2.au.reastatic.net/640x480/20bfc8668a30e8cabf045a1cd54814a9042fc715a8be683ba196898333d68cec/main.jpg'
-  };
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PropertyCardComponent {
+  @Input()
+  propertyDetail: Property;
 }
